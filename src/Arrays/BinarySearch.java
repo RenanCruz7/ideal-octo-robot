a0e020dea6bdca24144264a582e128e7b8b43f67
RenanCruz7 <renancruz199@gmail.com>
2024-12-03 22:54:32 -0300

    BinarySearch feita

---
+++ src/Arrays/BinarySearch.java
@@ -0,0 +1,24 @@
+package Arrays;
+
+public class BinarySearch {
+
+    public boolean binarySearch(int[] nums, int target) {
+        int left = 0;
+        int right = nums.length - 1;
+
+        while (left <= right) {
+            int mid = left + (right - left) / 2; // to avoid overflow
+
+            if (nums[mid] == target) {
+                return true;
+            } else if (nums[mid] < target) {
+                left = mid + 1;
+            } else {
+                right = mid - 1;
+            }
+        }
+
+        return false;
+    }
+
+}
